8fd250e25abf1cd9531a9d0c57a7e78255eba9a3
saikatsingh <mailmetosaikat676@gmail.com>
2025-01-10 10:49:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '29563132'))
 API_HASH = environ.get('API_HASH', 'b39be032fc0c567d0cda60dbea99606e')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7836574966:AAG3Rz4Xmy-h_W8F2VF3UEHi5raVnWXevY0")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
